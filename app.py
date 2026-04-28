f02e119bc5dd42137fc6f56ad4aee35c281dd891
Seu Nome <seuemail@gmail.com>
2026-04-28 10:44:02 -0300

    pequenos ajustes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
 from supabase import create_client
 from werkzeug.security import generate_password_hash, check_password_hash
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
+from functools import wraps
 import pandas as pd
 import time
 import os
+import io
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
